7452a8095b0681bc307ea79911491892ca7d1b0a
서인석 <isseo8010@gmail.com>
2025-09-07 19:02:18 +0900

    feat: 카카오 로그인 API 구현 및 오류 수정
    - NestJS 백엔드 카카오 인증 서비스 구현
    - 프론트엔드 axios 모듈 설정 및 HTTP 통신 오류 해결
    - Authorization 헤더 및 에러 처리 개선

---
+++ src/test/vue3-nest/nuxt-frontend/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <a @click="loginWithKakao">
+    <img src="/kakao_login.png"/>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  methods: {
+    kakaoInit () {
+      Kakao.init('a21fca6df45467b72c4cff24ec14cb6b')// KaKao client key
+      Kakao.isInitialized()
+    },
+    async loginWithKakao () {
+      await Kakao.Auth.authorize({
+        redirectUri: `${window.location.origin}/kakao-callback`
+      })
+    }
+  },
+  mounted () {
+    this.kakaoInit()
+  }
+}
+</script>
+
+
