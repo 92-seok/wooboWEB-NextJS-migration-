0428a3602d9d2256e7f5bc0f6736f35474f5f55b
DESKTOP-5OFTTR9\Admin <woobolab@woobosys.com>
2025-09-11 21:10:49 +0900

    Operation

--- src/Project/backend/src/main.ts
+++ src/Project/backend/src/main.ts
@@ -7,11 +7,11 @@ async function bootstrap() {
   console.log('DB_HOST:', process.env.DB_HOST);
   console.log('DB_PORT:', process.env.DB_PORT);
   console.log('DB_USERNAME:', process.env.DB_USERNAME);
-  console.log('DB_PASSWORD:', process.env.DB_PASSWORD ? '****' : 'undefined');
+  console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
   console.log('DB_DATABASE:', process.env.DB_DATABASE);
   
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 8080);
-  console.log(`🚀 애플리케이션이 http://localhost:${process.env.PORT ?? 8080} 에서 실행 중입니다`);
+  console.log(`🚀 애플리케이션이 http://localhost:${process.env.PORT} 에서 실행 중입니다`);
 }
 bootstrap();
--- src/Project/frontend/vue.config.js
+++ src/Project/frontend/vue.config.js
@@ -5,10 +5,14 @@ module.exports = defineConfig({
 
 module.exports = {
   devServer: {
+    port: 80,
+    host: '0.0.0.0',
+    allowedHosts: ['woobo.online'],
     proxy: {
       '/api': {
         target: 'http://localhost:8080', // 백엔드 서버 주소
       },
-    }
+    },
+
   }
 }
